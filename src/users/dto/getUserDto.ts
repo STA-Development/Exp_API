f62814171ae3094827762c7b2a9b6cce837f10d8
avo2003 <fishenkjianavo@gmail.com>
2022-04-27 19:31:43 +0400

    change swaggerUsers responses

---
+++ src/users/dto/getUserDto.ts
@@ -0,0 +1,44 @@
+import { User } from '../entity/user';
+import { Pivot } from '../../events/entity/pivot';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class GetUserDto extends User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  firstName: string;
+
+  @ApiProperty()
+  lastName: string;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty()
+  rating: number;
+
+  @ApiProperty()
+  performerType: string;
+
+  @ApiProperty()
+  authUid: string;
+
+  @ApiProperty()
+  isAdmin: boolean
+
+  @ApiProperty()
+  salary: number;
+
+  @ApiProperty()
+  avatar: string;
+
+  @ApiProperty()
+  avatarPublicId: string;
+
+  @ApiProperty()
+  position:string;
+
+  @ApiProperty()
+  pivot: Pivot[];
+}
